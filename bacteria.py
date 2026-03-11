b4d476f8fb9d631fa3dab64258117d9b6e9739ec
Xcicle <kaungsittyanrx2021@gmail.com>
2026-03-11 16:41:36 +0630

    UPDATE

--- bacteria.py
+++ bacteria.py
@@ -19,6 +19,48 @@ class Bacteria:
         with open('population.txt',mode='a') as f:
             f.write('\n'+str(self.population))
 
+    def calculate_births(self, env):
+
+        birth_modifier = 1
+        # -------------------- # TEMPERATURE EFFECT # --------------------
+        temp = env.temperature
+        if 35 <= temp <= 39:  # Optimal
+            birth_modifier *= 1.3
+        elif 30 <= temp < 35 or 39 < temp <= 42:  # Stress
+            birth_modifier *= 0.6
+        else:  # Lethal
+            birth_modifier *= 0.1
+        # -------------------- # pH EFFECT # --------------------
+        ph = env.pH_level
+        ph_diff = abs(ph - 7)
+        if ph_diff <= 1:  #
+            Optimal
+            birth_modifier *= 1.2
+        elif ph_diff <= 3:  # Stress
+            birth_modifier *= 0.5
+        else:  # Lethal
+            birth_modifier *= 0.05
+        # -------------------- # HUMIDITY EFFECT # --------------------
+        humidity = env.humidity
+        if 40 <= humidity <= 70:
+            birth_modifier *= 1.1
+        elif 25 <= humidity < 40:
+            birth_modifier *= 0.7
+        else:
+            birth_modifier *= 0.3
+        # -------------------- # NUTRIENTS EFFECT # --------------------
+        nutrients = env.nutrients
+        if nutrients >= 7:
+            birth_modifier *= 1.2
+        elif 3 <= nutrients < 7:
+            birth_modifier *= 0.7
+        else:
+            birth_modifier *= 0.2
+        # Logistic Growth Limit
+        carrying_capacity = 5000
+        logistic_factor = 1 - (self.population / carrying_capacity)
+        return self.population * self.base_birth_rate * birth_modifier * logistic_factor
+
     def calculate_deaths(self, env):
 
         death_modifier = 1
